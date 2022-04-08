d7ebeb6b368c191c58300b8b49eecdf2826f383e
Deborah Stebler <deborah.stebler@uzh.ch>
2022-04-08 16:00:48 +0200

    Added Zoom example

---
+++ src/zoom/js/config.js
@@ -0,0 +1,9 @@
+export default {
+    sdkKey: 'fWWnmIWXTLvmsjeWYEO5bOrlEYtutGTKRD4c',
+    sdkSecret: 'eA1E5aSVX5F3a2hSkayAWXxl1CI1UP2Ih9Su',
+    topic: 'The Game',
+    password: '',
+    name: 'Display Name',
+    sessionKey: '123',
+    user_identity: 'user123'
+};
