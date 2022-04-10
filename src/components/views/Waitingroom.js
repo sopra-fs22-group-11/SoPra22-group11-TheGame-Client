4426aee6d79ff92a0d6181752485f1d750184142
Deborah Stebler <deborah.stebler@uzh.ch>
2022-04-10 10:50:40 +0200

    Client Join now works

--- src/components/views/Waitingroom.js
+++ src/components/views/Waitingroom.js
@@ -11,25 +11,69 @@ import initClientEventListeners from "../../zoom/js/meeting/session/client-event
 import initButtonClickHandlers from "../../zoom/js/meeting/session/button-click-handlers";
 import state from "../../zoom/js/meeting/session/simple-state";
 import sessionConfig from "../../zoom/js/config";
+import VideoSDK from "@zoom/videosdk";
 
 const Waitingroom =  () => {
-    const [client, setClient] = useState(null);
+    const client = ZoomVideo.createClient();
+    const audioTrack = VideoSDK.createLocalAudioTrack();
+    const videoTrack = VideoSDK.createLocalVideoTrack();
     let mediaStream;
+    const canvas = document.querySelector('.video-canvas');
+/* Eher Experimentell - Bruche mer aber bestimmt öppis i die richtig
+    client.on('peer-video-state-change', async (payload) => {
+        const { action, userId } = payload;
+        if (action === 'Start') {
+            await mediaStream.renderVideo(canvas, userId, 1280,640, 50, 50, 2);
+        } else if (action === 'Stop') {
+            await mediaStream.stopRenderVideo(canvas);
+        }
+    });
 
-    const joinMeeting = () => {
-        joinMeetingAndEventListeners();
-        /*const signature = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfa2V5IjoiZldXbm1JV1hUTHZtc2plV1lFTzViT3JsRVl0dXRHVEtSRDRjIiwidHBjIjoiVGhlIEdhbWU0Iiwicm9sZV90eXBlIjowLCJ1c2VyX2lkZW50aXR5IjoidGVzdE5hbWUiLCJzZXNzaW9uX2tleSI6IiIsImlhdCI6MTY0OTQ5MjQ5MiwiZXhwIjoxNjQ5NDk5NjkyfQ.EfydnuAodvD8g155Z0dOesjzIJSRycy_l7WIwdX8yJs"
-        client.join(
-            'The Game4', // It is very important to always (in testing phase) use a new session name or do this: https://devforum.zoom.us/t/meeting-passcode-wrong-but-passcode-is-actual-y-correct/61479/2
-            signature,
-            'testName',
-            '',
-        ).then(() => {
-            console.log('Join meeting success');
-        }).catch((error) => {
-            console.error(error);
-        });*/
+    const captureOptions = {
+        cameraId: 'cameraId',
+        captureWidth: 640,
+        captureHeight: 360,
+    };
+    // start capture
+    stream.startVideo();
+    // stop capture
+    stream.stopVideo();
+*/
+    const videoSDKLibDir = '/node_modules/@zoom/videosdk/dist/lib';
+
+
+    const joinMeeting = async () => {
+        console.log("Let's see our client:")
+        console.log(client)
+
+        await initAndJoinSession()
+
+        initClientEventListeners(client, mediaStream);
+        console.log('======= Initializing client event handlers done =======');
+        await startAudioMuted();
+        console.log('======= Starting audio muted done =======');
+        await initButtonClickHandlers(client, mediaStream);
+        console.log('======= Initializing button click handlers done =======');
+
+        console.log('======= Session joined =======');
     }
+
+    const initAndJoinSession = async () => {
+        //await client.init('en-US',`${window.location.origin}${videoSDKLibDir}`)
+        await client.init('en-US', 'Global');
+        const signature = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfa2V5IjoiZldXbm1JV1hUTHZtc2plV1lFTzViT3JsRVl0dXRHVEtSRDRjIiwidHBjIjoiVGhlIEdhbWU1Iiwicm9sZV90eXBlIjowLCJ1c2VyX2lkZW50aXR5IjoidGVzdE5hbWUiLCJzZXNzaW9uX2tleSI6IiIsImlhdCI6MTY0OTU3NDY3MCwiZXhwIjoxNjQ5NTgxODcwfQ.j7-bo2TUldQGZHiQqGz0s0TwCGnHnZNI-J6-nHUDmZI"
+        try {
+            await client.join(
+                'The Game5', // It is very important to always (in testing phase) use a new session name or do this: https://devforum.zoom.us/t/meeting-passcode-wrong-but-passcode-is-actual-y-correct/61479/2
+                signature,
+                'testName',
+                '');
+            mediaStream = client.getMediaStream();
+            state.selfId = client.getSessionInfo().userId;
+        } catch (e) {
+            console.error(e);
+        }
+    };
 
     const getClients = () => {
         let participants = client.getAllUser();
@@ -53,55 +97,6 @@ const Waitingroom =  () => {
         });
     }
 
-    async function joinMeetingAndEventListeners (){
-        /*const signature = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfa2V5IjoiZldXbm1JV1hUTHZtc2plV1lFTzViT3JsRVl0dXRHVEtSRDRjIiwidHBjIjoiVGhlIEdhbWU0Iiwicm9sZV90eXBlIjowLCJ1c2VyX2lkZW50aXR5IjoidGVzdE5hbWUiLCJzZXNzaW9uX2tleSI6IiIsImlhdCI6MTY0OTQ5MjQ5MiwiZXhwIjoxNjQ5NDk5NjkyfQ.EfydnuAodvD8g155Z0dOesjzIJSRycy_l7WIwdX8yJs"
-        try{
-            client.join(
-            'The Game4', // It is very important to always (in testing phase) use a new session name or do this: https://devforum.zoom.us/t/meeting-passcode-wrong-but-passcode-is-actual-y-correct/61479/2
-            signature,
-            'testName',
-            '')
-            mediaStream = client.getMediaStream();
-            state.selfId = client.getSessionInfo().userId;
-
-        } catch (e) {
-            console.error(e);
-        } finally {
-            console.log('Join meeting success');
-        }*/
-        joinBeginning();
-
-        initClientEventListeners(client, mediaStream);
-        console.log('======= Initializing client event handlers =======');
-        await startAudioMuted();
-        console.log('======= Starting audio muted =======');
-        await initButtonClickHandlers(client, mediaStream);
-        console.log('======= Initializing button click handlers =======');
-
-        console.log('======= Session joined =======');
-    }
-
-   const joinBeginning = () => {
-        const signature = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfa2V5IjoiZldXbm1JV1hUTHZtc2plV1lFTzViT3JsRVl0dXRHVEtSRDRjIiwidHBjIjoiVGhlIEdhbWUiLCJyb2xlX3R5cGUiOjEsInVzZXJfaWRlbnRpdHkiOiJ1c2VyMTIzIiwic2Vzc2lvbl9rZXkiOiIxMjMiLCJpYXQiOjE2NDk1MTAxMzcsImV4cCI6MTY0OTUxNzMzN30.1pZxCBOLgTTJYDh5dXP-1mtjoGL9ZCW6eWu-b1HgJf4"
-        try{
-            client.join(
-                'The Game4', // It is very important to always (in testing phase) use a new session name or do this: https://devforum.zoom.us/t/meeting-passcode-wrong-but-passcode-is-actual-y-correct/61479/2
-                signature,
-                'testName',
-                '')
-            mediaStream = client.getMediaStream();
-            state.selfId = client.getSessionInfo().userId;
-        } catch (e) {
-            console.error(e);
-            console.log("we got a error when joining")
-        }  finally {
-        console.log('Join meeting success');
-    }
-       mediaStream = client.getMediaStream();
-       state.selfId = client.getSessionInfo().userId;
-    };
-
-
 
     const startAudioMuted = () => {
         //await mediaStream.startAudio();
@@ -116,30 +111,6 @@ const Waitingroom =  () => {
             mediaStream.muteAudio();
         }
     };
-
-    useEffect(() => {
-        // effect callbacks are synchronous to prevent race conditions. So we put the async function inside:
-        async function setUpClient() {
-            try {
-                const client = ZoomVideo.createClient();
-                await client.init('en-US', 'Global');
-                setClient(client);
-
-                console.log("Let's see our client:")
-                console.log(client)
-
-
-            } catch (error) {
-                console.error(`Something went wrong while joining the meeting: \n${handleError(error)}`);
-                console.error("Details:", error);
-                alert("Something went wrong while fetching the users! See the console for details.");
-            }
-        }
-
-        setUpClient();
-    }, []);
-
-
 
 
     // localStorage.setItem('gameId', ); Hier noch herausfinden wie wir schauen, dass leute nur in ihr spiel können
@@ -178,6 +149,11 @@ const Waitingroom =  () => {
                     className="meeting-control-button meeting-control-button__leave-session">
                 <i id="js-leave-session-icon" className="fas fa-phone"></i>
             </button>
+            <div id="js-video-view" className="container video-app hidden">
+                <canvas id="video-canvas" className="video-canvas" width="1280" height="640"></canvas>
+                <div className="container meeting-control-layer">
+                </div>
+            </div>
         </BaseContainer>
 
 
