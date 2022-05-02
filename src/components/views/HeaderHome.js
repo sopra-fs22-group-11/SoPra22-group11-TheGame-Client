a31556ae267aa893bdc6770c92d62a3c2cfbf4da
Deborah Stebler <deborah.stebler@uzh.ch>
2022-05-02 18:22:01 +0200

    Started edit page and fixed logout issue

--- src/components/views/HeaderHome.js
+++ src/components/views/HeaderHome.js
@@ -6,6 +6,8 @@ import goToHome from "./Waitingroom";
 import goToRulePage from "./Startpage";
 import doRegister from "./Login";
 import goToLogin from "./Registration";
+import {useHistory} from "react-router-dom";
+import {api, handleError} from "../../helpers/api";
 
 /**
  * This is an example of a Functional and stateless component (View) in React. Functional components are not classes and thus don't handle internal state changes.
@@ -43,11 +45,27 @@ const changeLocation = () =>{
 const logout = () => {
     //get a token
     localStorage.removeItem('token');
-    //get ID of user
-    let id = localStorage.getItem('ID');
-    localStorage.removeItem('ID')
-}
+    let id = localStorage.getItem('loggedInUser');
+    console.log('/session/'+ id)
+    try{
+        const response = api.get('/session/'+ id);}
+    catch (e){
+        console.log(e)
+    }
 
+    localStorage.removeItem('loggedInUser')
+    localStorage.removeItem('token')
+    localStorage.removeItem('username');
+    localStorage.removeItem('clickedStart');
+    localStorage.removeItem('gto')
+}
+const goToUserPage = () => {
+    console.log("Hello")
+    //const response = fetchUser()
+
+    //console.log(response.id)
+
+}
 
 
 
@@ -56,17 +74,20 @@ const HeaderHome = props => (
         <div className="header title">
             The Game |
         </div>
-        <div className="header-right">
+        <iv className="header-right">
             <a href="/startpage"
                onClick={() => goToHome()}
             >Home </a>
+            <a href="/editUser/"
+               onClick={() => goToUserPage()}
+            >Edit Profile</a>
             <a href="/rulePage"
                onClick={() => goToRulePage()}
             >Rules</a>
             <a href="/login"
                onClick={() => logout()}
             >Logout</a>
-        </div>
+        </iv>
     </div>
 );
 
--- src/components/views/ProfileSettings.js
+++ src/components/views/ProfileSettings.js
@@ -8,18 +8,169 @@ import PropTypes from "prop-types";
 import "styles/views/Home.scss";
 import HeaderHome from "./HeaderHome";
 
-const ProfileSettings = () => {
 
+const PlayerInformationField = props => {
+    return (
+        <div className="entry field">
+            <label className="entry label">
+                {props.label}
+            </label>
+            <input
+                className="entry input"
+                // All the other displayed attributes (expect birthday) are not nullable and therefore always displayed
+                placeholder= "Reset your password"
+                type = {props.type}
+                value={props.value}
+                disabled = {props.disabled}
+                onChange={e => props.onChange(e.target.value)}
+            />
+        </div>
+    );
+};
+
+
+const ProfileSettings = () => {
+    const [user, setUser] = useState(null)
+    const [isDisabled, setIsDisabled] = useState(true); // We are currently not editing
+    const [updatedUsername, setUpdatedUsername] = useState(null)
+    const [updatedPassword, setUpdatedPassword] = useState(null);
+    const [score, setScore] = useState(null)
+    const id = localStorage.getItem('loggedInUser')
+
+
+
+
+    const editUser = (user) =>{
+        // Change state to editable
+        setIsDisabled(!isDisabled);
+
+        //Change updated Username
+        setUpdatedUsername(user.username)
+    }
+
+
+
+
+    async function saveChanges()  {
+        // We make sure the username is not empty
+        if(!user.username){
+            alert("Username cannot be empty")
+        }
+
+        try {
+            // Put the updated user to the server
+            await api.put('/users/'+id, user);
+
+            await new Promise(resolve => setTimeout(resolve, 1000));
+        }
+        catch (error) {
+            // Handle errors
+
+            console.error(`Something went wrong while editing the user: \n${handleError(error)}`);
+            console.error("Details:", error);
+            alert(`Something went wrong during the editing: \n${handleError(error)}`);
+
+        }
+        finally{
+            // Change the state of editable back
+            setIsDisabled(!isDisabled);
+        }
+
+
+    }
+
+
+
+    useEffect(() => {
+        // effect callbacks are synchronous to prevent race conditions. So we put the async function inside:
+        async function fetchUser() {
+            try {
+
+                // Get a single users data and store it
+                const response = await api.get('/users/'+ id);
+                const score = await api.get('/users/'+ id + '/score')
+
+                await new Promise(resolve => setTimeout(resolve, 1000));
+
+                setUser(response.data);
+                setScore(score.data)
+                console.log(user)
+                console.log(score)
+
+            } catch (error) {
+                // Handle occuring errors
+                console.error(`Something went wrong while fetching the users: \n${handleError(error)}`);
+                console.error("Details:", error);
+                alert("Something went wrong while fetching the users! See the console for details.");
+            }
+        }
+
+        fetchUser();
+
+    });
+
+
+
+    let content = <Spinner/>;
+
+    if (user) {
+        content = (
+            <div className="editUser">
+                <PlayerInformationField
+                    label="Username"
+                    disabled={isDisabled}
+                    value={isDisabled ? user.username : updatedUsername}
+                    onChange={un =>setUpdatedUsername(un)}
+                />
+                <PlayerInformationField
+                    type = "text"
+                    label="Status"
+                    disabled={true}
+                    value={user.status}
+                />
+                <PlayerInformationField
+                    type = "text"
+                    label="Score"
+                    disabled={true}
+                    value={score}
+                />
+                <PlayerInformationField
+                    type = "text"
+                    label="password"
+                    disabled={isDisabled}
+                    value = {updatedPassword}
+                    onChange ={pw =>setUpdatedPassword(pw)}
+                />
+
+
+                <Button
+                    //disabled={!accessRights && !updatedUsername}
+                    width="50%"
+                    onClick={isDisabled ? () => editUser(user): () => saveChanges()}
+
+                >
+                    {isDisabled ? "Edit User" : "Save changes"}
+                </Button>
+
+            </div>
+        );
+    }
 
     return (
-        <div>
+    <div>
         <HeaderHome height="100"/>
-        <BaseContainer className = "Home container">
+        <BaseContainer className="home container">
+            <div className="home form">
+                <h1>Profile Page</h1>
 
+                {content}
+            </div>
         </BaseContainer>
-        </div>
+    </div>
 
-    ) ;
+
+
+    );
 
 }
 
