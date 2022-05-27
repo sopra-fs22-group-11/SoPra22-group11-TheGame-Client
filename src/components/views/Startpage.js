2a22387d2f737d185fe15a98ded0a3c8fb81cd4f
Deborah Stebler <deborah.stebler@uzh.ch>
2022-05-27 12:15:07 +0200

    Fixed reload in Waitingroom overview

--- src/components/views/Startpage.js
+++ src/components/views/Startpage.js
@@ -11,31 +11,9 @@ import HeaderHome from "./HeaderHome";
 import {connect} from "../utils/sockClient";
 
 
-// COPY FROM GAME SITE OF M1
-const User = ({user}) => (
-    <div className="user container">
-        <div className="user username">{user.username}</div>
-        <div className="user name">{user.name}</div>
-        <div className="user id">id: {user.id}</div>
-    </div>
-);
-
-
-User.propTypes = {
-    user: PropTypes.object
-};
-
 const Startpage = () => {
-    // use react-router-dom's hook to access the history
+
     const history = useHistory();
-
-    // define a state variable (using the state hook).
-    // if this variable changes, the component will re-render, but the variable will
-    // keep its value throughout render cycles.
-    // a component can have as many state variables as you like.
-    // more information can be found under https://reactjs.org/docs/hooks-state.html
-    const [users, setUsers] = useState(null);
-
 
     const goToWaitingroomOverview = () => {
         connect(()=> {
@@ -45,48 +23,9 @@ const Startpage = () => {
     }
 
 
-    // the effect hook can be used to react to change in your component.
-    // in this case, the effect hook is only run once, the first time the component is mounted
-    // this can be achieved by leaving the second argument an empty array.
-    // for more information on the effect hook, please see https://reactjs.org/docs/hooks-effect.html
-    useEffect(() => {
-        // effect callbacks are synchronous to prevent race conditions. So we put the async function inside:
-        async function fetchData() {
-            try {
-                const response = await api.get('/users');
-
-                // delays continuous execution of an async operation for 1 second.
-                // This is just a fake async call, so that the spinner can be displayed
-                // feel free to remove it :)
-                await new Promise(resolve => setTimeout(resolve, 1000));
-
-                // Get the returned users and update the state.
-                setUsers(response.data);
-
-                // This is just some data for you to see what is available.
-                // Feel free to remove it.
-                console.log('request to:', response.request.responseURL);
-                console.log('status code:', response.status);
-                console.log('status text:', response.statusText);
-                console.log('requested data:', response.data);
-
-                // See here to get more data.
-                console.log(response);
-            } catch (error) {
-                console.error(`Something went wrong while fetching the users: \n${handleError(error)}`);
-                console.error("Details:", error);
-                alert("Something went wrong while fetching the users! See the console for details.");
-            }
-        }
-
-        fetchData();
-    }, []);
-
-    let content = <Spinner/>;
 
 
-    if (users) {
-        content = (
+    let content = (
             <div className="home"
             >
                 <div display="block" justify-content="space-between">
@@ -120,7 +59,7 @@ const Startpage = () => {
                 </Button>
             </div>
         );
-    }
+
 
 
     return (
