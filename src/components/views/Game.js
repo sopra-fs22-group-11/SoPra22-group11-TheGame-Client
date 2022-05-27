afdf617cde1e197116bce6ddf74290ebbec50014
Sandra Rosch <sandra.rosch@uzh.ch>
2022-05-27 15:59:35 +0200

    Coomunication help modal now shown and can be closed with click on backdrop

--- src/components/views/Game.js
+++ src/components/views/Game.js
@@ -873,8 +873,8 @@ const Game = () => {
         </div>);
     }
 
-    function clickHelp(){
-        setModalHelpIsOpen(true);
+    function closeHelpModal(){
+        setModalHelpIsOpen(false);
     }
     const helpText = (
         <div>
@@ -919,7 +919,7 @@ const Game = () => {
                 </BaseContainer>
                 <BaseContainer className = "overlay">
                     {modalHelpIsOpen && <Modal text ={helpText}/>}
-                    {modalHelpIsOpen && <Backdrop clicked ={setModalHelpIsOpen(false)}/>}
+                    {modalHelpIsOpen && <Backdrop clicked = {closeHelpModal} />}
 
                 </BaseContainer>
                     <div className="game formGame">
@@ -1004,7 +1004,7 @@ const Game = () => {
                     <div className="new-Label">
                         Communication:
                             <img src="https://img.icons8.com/external-bearicons-detailed-outline-bearicons/64/FFFFFF/external-question-call-to-action-bearicons-detailed-outline-bearicons.png" width="40px"
-                                 onClick={() => clickHelp()}
+                                 onClick={() => setModalHelpIsOpen(true)}
                             />
 
                     </div>
