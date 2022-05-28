db22c8fe31877ca8955b5726ce73b97a30fde78e
Mariinja <marinja.principe@uzh.ch>
2022-05-28 14:20:53 +0200

    make a nice reload page

--- src/components/views/AfterReload.js
+++ src/components/views/AfterReload.js
@@ -1,13 +1,11 @@
 import {useHistory} from "react-router-dom";
-import {connect, isConnected, sock} from "../utils/sockClient";
 import {Button} from "../ui/Button";
 import HeaderHome from "./HeaderHome";
 import BaseContainer from "../ui/BaseContainer";
-import client from "./Game"
-import {Spinner} from "../ui/Spinner";
 import "styles/views/Home.scss";
 import "styles/ui/Button.scss";
-import React, {useEffect, useState} from "react";
+import React from "react";
+import ErrorChicken from '../../ErrorChicken.png';
 
 const AfterReload = () => {
     const history = useHistory();
@@ -17,9 +15,11 @@ const AfterReload = () => {
         <div>
             <HeaderHome height="100"/>
             <BaseContainer className="home container">
-                <h2> You are not allowed to reload, click the "x" on the tab or us the arrows during The Game. The Game has ended. </h2>
                 <div className="home form">
+                <h2> You are not allowed to reload, click the "x" on the tab or us the arrows during The Game. <br/> The Game has ended. </h2>
+
                     <div>
+                        <img src={ErrorChicken} className="home errorChicken"/> <br/>
                         <Button
                             width ="30%"
                             onClick={() => history.push("/startpage")}
@@ -28,6 +28,7 @@ const AfterReload = () => {
                         </Button>
                     </div>
              </div>
+                <a target="_blank" href="https://www.nicepng.com/downpng/u2w7a9y3r5o0a9e6_saying-clipart-chicken-chicken-with-stop-sign/" >Image Credit: Nicepng.com</a>
                 </BaseContainer>
         </div>
     )
